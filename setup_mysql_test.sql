5e628aa46e4ffff8df1744da692c1dcfde4aeda0
root <oloobrian89@gmail.com>
2023-09-19 13:47:47 +0000

    MySQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,12 @@
+-- Prepare the server for the project
+-- Create a database hbnb_test_db and a new user hbnb_test (in localhost)
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd'
+
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+GRANT SELECT
+	ON performance_schema.* to 'hbnb_test'@'localhost';
+
+FLUSH PRIVILEGES;
