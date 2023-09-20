7025fb17d181357e8e97e7b474ad6286e660a4bc
root <oloobrian89@gmail.com>
2023-09-20 13:41:31 +0000

    DBStorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,13 @@
 from os import getenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-from models.base_model import Base
+from sqlalchemy.orm import sessionmaker, scoped_session, relationship
+from models.base_model import BaseModel, Base
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class DBStorage:
