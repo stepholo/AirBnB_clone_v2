2220fe997dce44a84af1765e92bd81be767b8484
maggywairigu <68754799+maggywairigu@users.noreply.github.com>
2023-09-20 22:04:59 +0300

    Update place.py

--- models/place.py
+++ models/place.py
@@ -28,11 +28,14 @@ class Place(BaseModel, Base):
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     reviews = relationship(
             'Review', backref='place',
-            cascade='all, delete, delete-orphan')
+            cascade='delete')
+    amenities = relationship("Amenity", secondary="place_amenity",
+                            viewonly=False)
 
     amenity_ids = []
 
