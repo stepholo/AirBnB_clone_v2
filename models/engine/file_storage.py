379a2070de5667b44a5758d9310b767451918f1b
maggywairigu <wairigumaina2016@gmail.com>
2023-09-20 07:30:09 +0000

    file_storage updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,24 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage.
+        If cls is provided, returns a dictionary filtered by class type."""
+        if cls is not None:
+            filtered_objs = {}
+            for key, obj in self.__objects.items():
+                if isinstance(obj, cls):
+                    filtered_objs[key] = obj
+            return filtered_objs
+        else:
+            return self.__objects
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it exists."""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
