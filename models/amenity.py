d1d893e426d9a12553addffabe3561e7b1281149
maggywairigu <68754799+maggywairigu@users.noreply.github.com>
2023-09-20 21:38:05 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
+from os import getenv
 from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column
@@ -17,7 +19,12 @@ class Amenity(BaseModel, Base):
     name (sqlalchemy String): The amenity name.
     place_amenities (sqlalchemy relationship): Place-Amenity relationship.
     """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize Amenity"""
+        super().__init__(*args, **kwargs)
