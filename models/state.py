1f10a8a9ae0b3e73a783017b19c9476078f890e9
root <oloobrian89@gmail.com>
2023-09-20 10:47:42 +0000

    DBStorage - State

--- models/state.py
+++ models/state.py
@@ -2,11 +2,34 @@
 """ State Module for HBNB project """
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from models import storage_type
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """ State class / table model"""
+
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
+
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship(
+                "City", backref="state", 
+                cascade="all, delete, delete-orphan")
+    else:
+        name =''
+
+        @property
+        def cities(self):
+            """returns list of city instances with state_id
+                equals the current State.id
+                FileStorage relationship between State and City
+            """
+            from models import storage
+            cities_rel = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    cities_rel.append(city)
+            return cites_rel
