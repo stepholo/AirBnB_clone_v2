4d7f7816b4d9a037723d372228ad501577610c51
root <oloobrian89@gmail.com>
2023-09-20 11:32:15 +0000

    DBStorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,19 +4,28 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
 
 class DBStorage:
+    """Database storage engine for mysql storage"""
+    __engine = None
+    __session = None
+
     def __init__(self):
+        '''dbstorage instance instantiation'''
         self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
                                       format(getenv("HBNB_MYSQL_USER"),
                                              getenv("HBNB_MYSQL_PWD"),
                                              getenv("HBNB_MYSQL_HOST"),
                                              getenv("HBNB_MYSQL_DB")),
                                       pool_pre_ping=True)
+
         if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
 
-        self.__session = None
-
     def all(self, cls=None):
+        """query on the current db session all cls objects
+            this method must return a dictionary: (like FileStorage)
+            key = <class-name>.<object-id>
+            value = object
+        """
         if cls is None:
             # Query all classes
             classes = [State, City, User, Place, Review, Amenity]
@@ -35,16 +44,30 @@ class DBStorage:
         return result
 
     def new(self, obj):
-        self.__session.add(obj)
+        """adds the obj to the current db session"""
+        if obj is not None:
+            try:
+                self.__session.add(obj)
+                self.__session.flush()
+                self.__session.refresh(obj)
+            except Exception as e:
+                self.__session.rollback()
+                raise e
 
     def save(self):
+        """commit all changes to the current db session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        if obj:
-            self.__session.delete(obj)
+        """deletes from the current database session the obj
+            if it's not None
+        """
+        if obj is not None:
+            self.__session.query(type(obj)).filter(
+                    type(obj).id == obj.id).delete()
 
     def reload(self):
+        """reloads the database"""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
@@ -52,4 +75,5 @@ class DBStorage:
         self.__session = Session()
 
     def close(self):
+        """closes the working SQLAlchemy session"""
         self.__session.close()
