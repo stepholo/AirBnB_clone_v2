e7df5b3bc60e8a01d1aebf0328d5e355aeedb264
maggywairigu <wairigumaina2016@gmail.com>
2023-09-21 08:36:15 +0000

    console.py update

--- console.py
+++ console.py
@@ -143,7 +143,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
         except NameError:
             print("** class doesn't exist **")
-    
+
     def help_create(self):
         """ Help information for the create method """
         print("Creates a class of any type")
@@ -205,7 +205,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -337,6 +337,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
