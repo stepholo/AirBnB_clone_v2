77ae2f77ffd0bea33d41b089617e0ff86ab6e87d
stepholo <oloobrian89@gmail.com>
2023-10-05 02:28:37 -0700

    Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import put, env, task, run
 import os
 
 env.user = 'ubuntu'
-env.hosts =['54.87.160.173', '54.162.87.193']
+env.hosts = ['54.87.160.173', '54.162.87.193']
 env.key_filename = '~/.ssh/school'
 
 
@@ -15,21 +15,24 @@ def do_deploy(archive_path):
         Param:
             archive_path: Path to the archived file
     """
-    if os.path.exists(archive_path) == False:
+    if os.path.exists(archive_path) is False:
         return False
 
     f_name = os.path.basename(archive_path)
     basef_name = os.path.splitext(f_name)[0]
-    remote_path =  f"/tmp/{f_name}"
+    remote_path = f"/tmp/{f_name}"
+
+    target_d = f"/data/web_static/releases/{basef_name}"
+    link = "/data/web_static/current"
 
     put(archive_path, remote_path)
     run(f'mkdir -p /data/web_static/releases/{basef_name}/')
     run(f'tar -xzf /tmp/{f_name} -C /data/web_static/releases/{basef_name}')
     run(f'rm /tmp/{f_name}')
-    run(f'mv /data/web_static/releases/{basef_name}/web_static/* /data/web_static/releases/{basef_name}')
+    run(f'mv /data/web_static/releases/{basef_name}/web_static/* {target_d}')
     run(f'rm -rf /data/web_static/releases/{basef_name}/web_static')
-    run('rm -rf /data/web_static/current')
-    run(f'ln -s /data/web_static/releases/{basef_name} /data/web_static/current')
+    run(f'rm -rf {link}')
+    run(f'ln -s /data/web_static/releases/{basef_name} {link}')
 
     print("New version deployed!")
     return True
