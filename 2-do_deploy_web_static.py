d706b4cdaabbdc7f5e6f169da09ea80ccee1ae4d
stepholo <oloobrian89@gmail.com>
2023-10-05 04:25:26 -0700

    Deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" Module to distribute an archive to the web server"""
+"""Module to define a fab file"""
 
-from fabric.api import put, env, task, run
+from fabric.api import put, env, run
 import os
 
 env.user = 'ubuntu'
@@ -9,12 +9,12 @@ env.hosts = ['54.87.160.173', '54.162.87.193']
 env.key_filename = '~/.ssh/school'
 
 
-@task
 def do_deploy(archive_path):
     """Function to distribute an archive to your web servers
         Param:
             archive_path: Path to the archived file
     """
+
     if os.path.exists(archive_path) is False:
         return False
 
@@ -24,15 +24,25 @@ def do_deploy(archive_path):
 
     target_d = f"/data/web_static/releases/{basef_name}"
     link = "/data/web_static/current"
+    mv = f"/data/web_static/releases/{basef_name}/web_static/*"
+    rm = f"/data/web_static/releases/{basef_name}/web_static"
 
-    put(archive_path, remote_path)
-    run(f'mkdir -p /data/web_static/releases/{basef_name}/')
-    run(f'tar -xzf /tmp/{f_name} -C /data/web_static/releases/{basef_name}')
-    run(f'rm /tmp/{f_name}')
-    run(f'mv /data/web_static/releases/{basef_name}/web_static/* {target_d}')
-    run(f'rm -rf /data/web_static/releases/{basef_name}/web_static')
-    run(f'rm -rf {link}')
-    run(f'ln -s /data/web_static/releases/{basef_name} {link}')
+    if put(archive_path, remote_path).failed is True:
+        return False
+    if run(f'mkdir -p /data/web_static/releases/{basef_name}/').failed is True:
+        return False
+    if run(f'tar -xzf /tmp/{f_name} -C {target_d}').failed is True:
+        return False
+    if run(f'rm /tmp/{f_name}').failed is True:
+        return False
+    if run(f'mv {mv} {target_d}').failed is True:
+        return False
+    if run(f'rm -rf /{rm}').failed is True:
+        return False
+    if run(f'rm -rf {link}').failed is True:
+        return False
+    if run(f'ln -s {target_d} {link}').failed is True:
+        return False
 
     print("New version deployed!")
     return True
